b542019fb026b0d153168d0b15fbd7991bb9052d
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-10-28 10:02:54 +0530

    updated4

--- Backend/app.py
+++ Backend/app.py
@@ -1239,4 +1239,4 @@ def calculate_sentiment_analysis_multichannels():
 #     app.run(debug=True)
 # # Run the app for deployment
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
