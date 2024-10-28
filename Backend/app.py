2fcdeae8288420079152f3349940f6f43fe4436b
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-10-28 09:49:05 +0530

    updated2

--- Backend/app.py
+++ Backend/app.py
@@ -48,7 +48,11 @@ from google.auth.transport.requests import AuthorizedSession
 # Step 5: Build the YouTube client using the authorized session
 youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
 
-CORS(app, resources={r"/*": {"origins": "https://tube-metric-full-stack.vercel.app/"}})
+# CORS(app, resources={r"/*": {"origins": "https://tube-metric-full-stack.vercel.app"}})
+CORS(app, resources={r"/api/*": {"origins": "https://tube-metric-full-stack.vercel.app"}})
+# CORS(app, resources={r"/*": {"origins": "https://tube-metric-full-stack.vercel.app"}})
+
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 
