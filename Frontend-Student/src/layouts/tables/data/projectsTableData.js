a00773821e06dab2093498ed91022b15d7b17d3c
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-10-28 10:29:15 +0530

    updated8

--- Frontend-Student/src/layouts/tables/data/projectsTableData.js
+++ Frontend-Student/src/layouts/tables/data/projectsTableData.js
@@ -38,7 +38,7 @@ export default function data() {
 
       try {
         // const response = await fetch(`http://127.0.0.1:5000/api/channel/${currentYoutubeId}`);
-        const response = await fetch(`https://tube-metrics-full-stack.onrender.com${currentYoutubeId}`);
+        const response = await fetch(`https://tube-metrics-full-stack.onrender.com/api/channel/${currentYoutubeId}`);
         const data = await response.json();
         if (response.ok) {
           setSubscriberCount(data.channel_details?.subscriberCount || 0);
